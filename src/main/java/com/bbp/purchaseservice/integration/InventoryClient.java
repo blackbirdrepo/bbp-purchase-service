29148590db0b896f029d764a1e8cf437a3a537cd
Oleh Sheketa <oleh.sheketa@gmail.com>
2019-06-30 00:47:28 +0300

    added hystrix fallbacks

--- src/main/java/com/bbp/purchaseservice/integration/InventoryClient.java
+++ src/main/java/com/bbp/purchaseservice/integration/InventoryClient.java
@@ -2,6 +2,7 @@ package com.bbp.purchaseservice.integration;
 
 import com.bbp.purchaseservice.integration.dto.InventoryDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import static com.bbp.purchaseservice.config.Constants.WAREHOUSE_PATH;
 
-@FeignClient("bbp-warehouse-service")
+@FeignClient(value = "bbp-warehouse-service", fallback = InventoryClient.InventoryFallback.class)
 @Service
 public interface InventoryClient {
     //TODO implement fallback
@@ -20,4 +21,18 @@ public interface InventoryClient {
     @PostMapping(path = WAREHOUSE_PATH + "/inventories/{inventoryId}/quantity/reduce")
     void reduceInventoryQuantity(@PathVariable("inventoryId") String inventoryId,
                                  @RequestParam("amount") int amount);
+
+    @Component
+    public static class InventoryFallback implements InventoryClient {
+
+        @Override
+        public InventoryDto readInventory(String inventoryId) {
+            return new InventoryDto();
+        }
+
+        @Override
+        public void reduceInventoryQuantity(String inventoryId, int amount) {
+
+        }
+    }
 }
